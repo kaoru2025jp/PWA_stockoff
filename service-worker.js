be1901907a47bb10fb50a39f89985ee9e07eecd1
kaoru2025jp <kaorusaku1@gmail.com>
2025-12-27 17:02:39 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'stockoff29-v1';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
